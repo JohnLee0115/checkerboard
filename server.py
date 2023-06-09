6232972309e2204910c6a3fadc1aa3440643a4db
johnlee0115 <johnlee011500@gmail.com>
2023-06-09 14:33:02 -0700

    update

--- server.py
+++ server.py
@@ -1,18 +1,13 @@
 from flask import Flask, render_template  # added render_template!
 app = Flask(__name__)
 
-
 @app.route('/')
-def eight_by_eight(num1 = 8, num2 = 8):
-    return render_template('index.html', num1 = num1, num2 = num2) 
-
 @app.route('/<int:num1>')
-def eight_by_num1(num1, num2 = 8):
-    return render_template('index.html', num1 = num1, num2 = num2)
 @app.route('/<int:num1>/<int:num2>')
-def num1_by_num2(num1, num2):
-    return render_template('index.html', num1 = num1, num2 = num2)
-
+@app.route('/<int:num1>/<int:num2>/<string:color1>')
+@app.route('/<int:num1>/<int:num2>/<string:color1>/<string:color2>')
+def color_by_color(num1 = 8, num2 = 8, color1 = 'red', color2 = 'black'):
+    return render_template('index.html', num1 = num1, num2 = num2, color1 = color1, color2 = color2)
 
 if __name__=="__main__":
     app.run(debug=True)
